02491be6e2cdab03a4b560b6469a027d3761361f
flavioalvaia-lab <flavio.alvaia@aluno.senai.br>
2026-09-11 08:59:56 -0300

    feat(documentos): criacao diretorio upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -18,7 +18,15 @@ namespace BancoSENAIAPI.Controllers
         [HttpPost("upload/{codigoCliente}")]
         public async Task<IActionResult> AnexarArquivo(int codigoCliente, IFormFile arquivo)
         {
+            if (arquivo == null || arquivo.Length == 0)
+            {
+                return BadRequest("Nenhum arquivo foi enviado");
 
+            }
+
+            string pastaCliente
         }
     }
 }
+
+            
