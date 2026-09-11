f7f10c00494cbc8e5d3223d38f542224526742d1
flavioalvaia-lab <flavio.alvaia@aluno.senai.br>
2026-09-11 09:16:46 -0300

    feat(documentos): salvando arquivo no upload

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -26,12 +26,20 @@ namespace BancoSENAIAPI.Controllers
 
             string pastaCliente = Path.Combine(_caminhoRaiz, codigoCliente.ToString());
 
-            if(!Directory.Exists(pastaCliente))
+            if (!Directory.Exists(pastaCliente))
             {
                 Directory.CreateDirectory(pastaCliente);
             }
-        }
-    }
+
+            string extensao = Path.GetExtension(arquivo.FileName);
+            string nomeOriginal Path.GetFileNameWithoutExtension(arquivo.FileName);
+            string novoNome = $"{codigoCliente}_{nomeOriginal}_{Guid.NewGuid()}{extensao}";
+            string caminhoFinal Path.Combine(pastaCliente, novoNome);
+            using (var stream = new FileStream(caminho Final, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
+        } }
 }
 
             
