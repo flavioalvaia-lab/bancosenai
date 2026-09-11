595295e6e4cd2b0200fc812fe8508f3c87a25e61
flavioalvaia-lab <flavio.alvaia@aluno.senai.br>
2026-09-11 08:39:09 -0300

    Update DocumentoController.cs

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -6,6 +6,13 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentoController : Controller
     {
-        
+        private readonly string _caminhoRaiz = Path.Combine(
+            Directory.GetCurrentDirectory(),
+            "ClienteArquivos"
+            );
+
+        private static List<Models.DocumentoMetado> _documentosMetadados = new List<Models.DocumentMetadados>();
+
+        private static int _nextId = 1;
     }
 }
